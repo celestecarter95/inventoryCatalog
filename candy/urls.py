224a2336fa1fdc5bed1d95edca1ff93bddada82c
Celeste Carter <ccarte39@dmail.dixie.edu>
2015-10-05 11:36:14 -0600

    Got all functionality to work, now trying to get images to appear

--- candy/urls.py
+++ candy/urls.py
@@ -1,9 +1,17 @@
 from django.conf.urls import url
 
+from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
 	url(r'^$', views.CategoryListView.as_view(), name='index'),
 	url(r'^category/(?P<pk>[0-9]+)/$', views.CategoryDetailView.as_view(), name='categorydetail'),
 	url(r'^item/(?P<pk>[0-9]+)/$', views.ItemDetailView.as_view(), name='itemdetail'),
+	url(r'^category/add/$', views.CreateCategoryView.as_view(), name='addcategory'),
+	url(r'^category/(?P<pk>[0-9]+)/update/$', views.UpdateCategoryView.as_view(), name='updatecategory'),
+	url(r'^category/(?P<pk>[0-9]+)/delete/$', views.DeleteCategoryView.as_view(), name='deletecategory'),
+	url(r'^category/delete/success/$', TemplateView.as_view(template_name='candy/success.html'), name='deletecategorysuccess'),
+	url(r'^item/add/$', views.CreateItemView.as_view(), name='additem'),
+	url(r'^item/(?P<pk>[0-9]+)/update/$', views.UpdateItemView.as_view(), name='updateitem'),
+	url(r'^item/(?P<pk>[0-9]+)/delete/$', views.DeleteItemView.as_view(), name='deleteitem'),
 ]
--- candy/views.py
+++ candy/views.py
@@ -17,3 +17,46 @@ class CategoryDetailView(generic.DetailView):
 
 class ItemDetailView(generic.DetailView):
 	model = Item
+
+class CreateCategoryView(CreateView):
+	#template_name is "candy/category_form.html"
+	model = Category
+	fields = ['category', 'name', 'description']
+
+	def get_success_url(self):
+		return reverse('candy:categorydetail', args=(self.object.pk,))
+
+class UpdateCategoryView(UpdateView):
+	#template_name is "candy/category_form.html"
+	model = Category
+	fields = ['category', 'name', 'description']
+
+	def get_success_url(self):
+		return reverse('candy:categorydetail', args=(self.object.pk,))
+
+class DeleteCategoryView(DeleteView):
+	#tempalte_name is "category_confirm_delete.html"
+	model = Category
+	success_url = reverse_lazy('candy:deletecategorysuccess')
+	
+class CreateItemView(CreateView):
+	#template_name is "candy/item_form.html"
+	model = Item
+	fields = ['name', 'quantity', 'description', 'category']
+	def get_success_url(self):
+		return reverse('candy:categorydetail', args=(self.object.category.id,))
+
+class UpdateItemView(UpdateView):
+	#template_name is "candy/item_form.html"
+	model = Item
+	fields = ['name', 'quantity', 'description', 'category']
+
+	def get_success_url(self):
+		return reverse('candy:categorydetail', args=(self.object.category.id,))
+
+class DeleteItemView(DeleteView):
+	#tempalte_name is "item_confirm_delete.html"
+	model = Item
+
+	def get_success_url(self):
+		return reverse('candy:categorydetail', args=(self.object.category.id,))
