98226d91f8da442d734a992cabc9ef2dbdb3d894
Celeste Carter <ccarte39@dmail.dixie.edu>
2015-10-05 12:19:04 -0600

    Change MEDUA to MEDIA so it should work now.

--- inventoryCatalog/settings.py
+++ inventoryCatalog/settings.py
@@ -104,7 +104,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDUA_URL = '/media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 THUMBNAIL_DEBUG = True
--- inventoryCatalog/urls.py
+++ inventoryCatalog/urls.py
@@ -22,5 +22,5 @@ from django.conf.urls.static import static
 urlpatterns = [
 	url(r'^', include('candy.urls', namespace='candy')),
     url(r'^admin/', include(admin.site.urls)),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
